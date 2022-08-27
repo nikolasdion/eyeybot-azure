dbfaa928aa5c4ac1d6f67593fe8289acf98b1e4a
Dion Susanto <nikolasdion@gmail.com>
2022-08-27 20:39:16 +0100

    Add echo function

--- EyEyBot/index.ts
+++ EyEyBot/index.ts
@@ -1,17 +1,37 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { ServerResponse } from "http";
+import { Telegraf } from "telegraf";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const BOT_TOKEN = process.env.BOT_TOKEN ?? "";
+const WEBHOOK_ADDRESS = process.env.WEBHOOK_ADDRESS ?? "";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+const bot = new Telegraf(BOT_TOKEN, { telegram: { webhookReply: true } });
 
+bot.telegram.setWebhook(WEBHOOK_ADDRESS);
+
+bot.on("text", (context) => {
+  const echo = getEcho(context.message.text);
+  if (echo) context.reply(echo);
+});
+
+const getEcho = (text: string): string | undefined => {
+  let echo;
+
+  const echoedWords = ["ey", "ea", "gelow", "anying"];
+  echoedWords.forEach((word) => {
+    const firstChars = text.substring(0, word.length);
+    if (firstChars.toLowerCase() === word) echo = firstChars;
+  });
+
+  return echo;
+};
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log(`HTTP trigger function processed a request ${req}`);
+  bot.handleUpdate(req.body, context.res as ServerResponse);
 };
 
 export default httpTrigger;
